5e77860a572c7d851497a4cb5b5b2038ca08155d
sazzad <sazzadador@gmail.com>
2025-02-15 17:29:05 +0600

    ratio plus minus

---
+++ ratio-plus-minus.js
@@ -0,0 +1,21 @@
+function plusMinus(arr) {
+  // Write your code here
+  let positive = 0;
+  let negative = 0;
+  let zero = 0;
+
+  for (let num of arr) {
+    if (num > 0) {
+      positive++;
+    } else if (num < 0) {
+      negative++;
+    } else {
+      zero++;
+    }
+  }
+  console.log((positive / arr.length).toFixed(6));
+  console.log((negative / arr.length).toFixed(6));
+  console.log((zero / arr.length).toFixed(6));
+}
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
